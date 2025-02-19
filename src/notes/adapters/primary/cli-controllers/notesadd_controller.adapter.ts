1ab174c0c174eda8b6c7fc7cae999ce180caaab7
Ahmad Saleh <dev.ahmadsaleh@gmail.com>
2025-02-19 17:10:55 +0200

    adding the delete note controller using CLI

--- src/notes/adapters/primary/cli-controllers/notesadd_controller.adapter.ts
+++ src/notes/adapters/primary/cli-controllers/notesadd_controller.adapter.ts
@@ -5,8 +5,11 @@ import { ControllerInputPort } from '@core/ports/input/controller/controller.por
 import { HttpResponseOutputPort } from '@core/ports/output/http/http_response.port'
 import { ObjectKeyExist } from '@shared/validations/object_validation'
 import { NoteAddRequestModel } from 'src/notes/models/request/noteadd.request.model'
+import { NoteDeleteRequestModel } from 'src/notes/models/request/notedelete.request.model'
 import { NoteAddResponseModel } from 'src/notes/models/response/inputadd.response.model'
+import { NoteDeleteResponseModel } from 'src/notes/models/response/inputDelete.response.model'
 import { NoteAddUseCase } from 'src/notes/usecases/noteAdd_usecase'
+import { NoteDeleteUseCase } from 'src/notes/usecases/noteDelete_usecase'
 
 export class NoteAddCliController implements ControllerInputPort<NoteAddResponseModel, NoteAddRequestModel> {
   constructor (
@@ -36,6 +39,28 @@ export class NoteAddCliController implements ControllerInputPort<NoteAddResponse
       )
     } catch (error) {
       // Handle errors
+      throw RequestValidationErrorAdapter.handleError(String(error))
+    }
+  }
+}
+
+export class NoteDeleteCliController implements ControllerInputPort<NoteDeleteResponseModel, NoteDeleteRequestModel> {
+  constructor (
+    private readonly usecase: NoteDeleteUseCase,
+    private readonly presenter: HttpResponseOutputPort<NoteDeleteResponseModel>
+  ) {}
+
+  async handleCommand (
+    command: CLICommandsModel<NoteDeleteRequestModel>
+  ): Promise<ResponseModel<NoteDeleteResponseModel>> | never {
+    try {
+      if (!ObjectKeyExist(command, 'content')) {
+        throw RequestValidationErrorAdapter.handleError('Invalid body Request')
+      }
+      const { content } = command
+      const noteId = await this.usecase.exec(content.id)
+      return await this.presenter.handleResponse(noteId, 'Note Has Been Deleted Successfully')
+    } catch (error) {
       throw RequestValidationErrorAdapter.handleError(String(error))
     }
   }
